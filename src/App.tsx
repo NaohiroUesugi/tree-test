201eff296a8293ff068f2e25ebc22f516a729a6f
NaohiroUesugi <nhd_8991.4070@icloud.com>
2021-04-16 15:37:27 +0900

    feat: add tree-test

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Tree, { TreeProps } from "rc-tree";
+import TreeNode from "rc-tree/lib/TreeNode";
 
+type Node = {
+  id: string;
+  type: "VIEW" | "IMAGE";
+  name: string;
+  parentId: string | null;
+  children: string[];
+};
+
+const tree: { [key: string]: Node } = {
+  "0": {
+    id: "0",
+    type: "VIEW",
+    name: "test1-0",
+    parentId: null,
+    children: ["1-0", "2-0"],
+  },
+  "1-0": {
+    id: "1-0",
+    type: "VIEW",
+    name: "test1-0",
+    parentId: "0",
+    children: ["1-1"],
+  },
+  "1-1": {
+    id: "1-1",
+    type: "IMAGE",
+    name: "test1-1",
+    parentId: "1-0",
+    children: [],
+  },
+  "2-0": {
+    id: "2-0",
+    type: "IMAGE",
+    name: "test2-0",
+    parentId: "0",
+    children: [],
+  },
+};
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Tree showIcon={true} switcherIcon={<span>click: </span>}>
+        {layerTreeNode(tree["0"])}
+      </Tree>
     </div>
   );
 }
+
+const layerTreeNode = (node: Node): JSX.Element => {
+  const res = node.children.map((id) => {
+    const item = tree[id];
+    if (item.children) {
+      return (
+        <TreeNode title={item.name} key={item.id} isLeaf={false}>
+          {layerTreeNode(item)}
+        </TreeNode>
+      );
+    } else {
+      return <TreeNode title={item.name} key={item.id} isLeaf={true} />;
+    }
+  });
+  return <>{res}</>;
+};
 
 export default App;
